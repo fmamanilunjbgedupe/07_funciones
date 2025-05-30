99b24860aa6244e8693d027662d8672b620ac602
felix <fmamanil@unjbg.edu.pe>
2025-05-30 10:14:37 -0500

    mejora del ejercicio3: opccion de mostrar la suma de los terminos de fibonacci

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,9 +1,12 @@
  #include<iostream>
 using namespace std;
 int main(){
-	int nmr,ter1,ter2,i,fibo,sumf;
+	int nmr,ter1,ter2,i,fibo,sumf,op;
 	cout<<"Ingrese el numero de terminos de la serie de fibonacci que desea que se muestre: ";
 	cin>>nmr;
+	cout<<"desea ver la suma de los terminos de la serie de fibonacci?"<<endl;
+	cout<<"de ser asi ingrese 1 y en caso contrario ingrese 0"<<endl;
+	cin>>op;
 	ter1=1;
 	ter2=0;
 	fibo=0;
@@ -15,5 +18,11 @@ int main(){
 		cout<<fibo<<",";
 		sumf=sumf+fibo;
     }
+    if(op!=1){
+	}
+	else{
+		cout<<endl<<"la suma de la serie de fibonacci es :"<<sumf;
+		
+	}
 	return 0;
 }
