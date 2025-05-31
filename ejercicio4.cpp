947ca9c066ae0e254a3e2f33d0e35af109a65a5b
felix <fmamanil@unjbg.edu.pe>
2025-05-31 16:50:09 -0500

    mejora del ejercicio4: se agrego validacion de los numeros ingresados, y el por que del resultado, en caso sea 0

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -4,13 +4,19 @@ int factorial(int);//prototipo
 int main(){
 	int m,l,n,c;
 	cout<<"para realizar la combinatoria:"<<endl;
+	cout<<"ingrese valores mayores a 0 o se volvera a solicitar que ingrese valores para m y n"<<endl;
+	do{
 	cout<<"ingrese el valor de m:"<<endl;
 	cin>>m;
 	cout<<"ingrese el valor de n"<<endl;
 	cin>>n;
+    }while(m<=0||n<=0);
 	l=m-n;
 	c=factorial(m)/(factorial(n)*factorial(l));
-	cout<<"el resultado de la combinatoria es :  "<<c;
+	cout<<"el resultado de la combinatoria es :  "<<c<<endl;
+	if(c==0){
+		cout<<"las respuesta es cero ya que "<<m<<"-"<<n<<" da negativo o da 0"<<endl;
+	}
 	return 0;
 }
 int factorial (int n){
