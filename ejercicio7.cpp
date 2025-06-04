2a98ffe26ea2a39a3e9465bdb707866a219811ee
felix <fmamanil@unjbg.edu.pe>
2025-06-03 22:46:09 -0500

    mejora del ejercicio7: opccion de volver a realizar la operacion

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -8,13 +8,18 @@ int mcm( int a , int b){
 	
 }
 int main(){
-	int num1,num2;
+	int num1,num2,op;
+	op=1;
+	while(op==1){
 	cout<<"ingrese el primer numero: ";
 	cin>>num1;
 	cout<<"ingrese el segundo numero: ";
 	cin>>num2;
 	cout<<"máximo comun divisor (mcd): "<<mcd(num1,num2)<<endl;
 	cout<<"minimo comun divisor (mcm): "<<mcm(num1,num2)<<endl;
+	cout<<"ingrese 1 si quiere realizar la misma operacion con otros numeros de caso contrario ingrese 0 o tro numero"<<endl;
+	cin>>op;
+	}
 	
 	return 0;
 }
